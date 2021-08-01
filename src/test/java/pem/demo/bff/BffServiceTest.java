4fcaa0d355d463839757e5983c2a7bd450628568
최창준 <aowert6518@gmail.com>
2021-08-01 16:46:20 +0900

    [test] csv파일로 bff데이터 저장 - csv파일로 불러와서 bff 저장하기 테스트 - 알게된 점 : cascade때문에 member가 중복 저장되는 문제가 발생(더 공부 필요)

---
+++ src/test/java/pem/demo/bff/BffServiceTest.java
@@ -0,0 +1,63 @@
+package pem.demo.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pem.demo.member.Member;
+import pem.demo.member.MemberService;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BffServiceTest {
+
+    @Autowired
+    BffRepository bffRepository;
+    @Autowired
+    MemberService memberService;
+    @Test
+    public void excelfileTest() {
+//        List<List<String>> ret = new ArrayList<List<String>>();
+        BufferedReader br = null;
+
+        try{
+            br = Files.newBufferedReader(Paths.get("C:\\Users\\ChangJun.Choi\\Desktop\\쥬피터홈\\bff docs\\bff docs\\bff.csv"));
+            //Charset.forName("UTF-8");
+            String line = "";
+            String firstLine = br.readLine();
+            System.out.println(firstLine);
+            while ((line = br.readLine()) != null) {
+                ArrayList<Float> bffFeatures = new ArrayList<>();
+                String array[] = line.split(",");
+                String name = array[0];
+                Member member = memberService.findUserByUserName(name);
+                for (int i = 1; i < array.length; i++) {
+                    float bffFeature = Float.parseFloat(array[i]);
+                    bffFeatures.add(bffFeature);
+                }
+                bffRepository.save(new Bff(member, bffFeatures));
+                break;
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally{
+            try{
+                if(br != null){
+                    br.close();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
