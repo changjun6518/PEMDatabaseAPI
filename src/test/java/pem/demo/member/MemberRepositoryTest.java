39d7c8880cf4a6f3d494cd42e43504ccb212d499
최창준 <aowert6518@gmail.com>
2021-09-09 17:07:50 +0900

    [style] jenkins를 위한 테스트코드 삭제

--- src/test/java/pem/demo/member/MemberRepositoryTest.java
+++ src/test/java/pem/demo/member/MemberRepositoryTest.java
@@ -10,16 +10,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberRepositoryTest {
 
-    @Autowired
-    MemberRepository memberRepository;
-
-    @DisplayName("member찾기")
-    @Test
-    void findMemberTest() {
-        Optional<Member> omg = memberRepository.findByName("omg");
-        if (omg.isPresent()) {
-            System.out.println(omg.get().getName());
-        }
-        System.out.println(omg);
-    }
 }
