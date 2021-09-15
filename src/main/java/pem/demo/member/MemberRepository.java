f6b5f22e2ad5f5f8a1941f6dd531eee22d59da80
최창준 <aowert6518@gmail.com>
2021-09-15 16:05:48 +0900

    [feat] OS에 따른 파일 디렉토리 설정 & Memberrepository 변경 - os는 개인 환경 window와 가상 환경 unix에 따라 파일 저장을 위해 os 구분으로 폴더 위치를 다르게 하여 저장 - 나중에 다시 리팩토링 필요
    - @Param을 넣어서 Query를 제대로 날리게 끔 함

--- src/main/java/pem/demo/member/MemberRepository.java
+++ src/main/java/pem/demo/member/MemberRepository.java
@@ -2,11 +2,12 @@ package pem.demo.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.name = :name")
-    Optional<Member> findByName(String name);
+    Optional<Member> findByName(@Param("name") String name);
 }
